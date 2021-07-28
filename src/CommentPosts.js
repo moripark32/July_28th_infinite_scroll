1a6451e398be646edf0c4d4b461d531d1e4130a8
moripark32 <moripark32@gmail.com>
2021-07-28 19:01:46 +0900

    feat:스크롤 기능 추가

--- src/CommentPosts.js
+++ src/CommentPosts.js
@@ -1,53 +1,111 @@
 import React, { useState, useEffect } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
 
-function App() {
+function CommentPosts() {
   const [comments, setComments] = useState([]);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/comments')
-      .then(({ data }) => setComments(data));
+    getData();
   }, []);
 
+  const getData = () => {
+    axios
+      .get(
+        `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`,
+      )
+      .then(({ data }) => {
+        setPage(prev => {
+          return prev + 1;
+        });
+        setComments(prev => {
+          const newArr = [...prev, ...data];
+          return newArr;
+        });
+      });
+  };
+
   return (
-    <Container>
-      <GlobalStyle />
-      {comments.map((post, index) => (
-        <Comment key={index}>
-          <Id>{post.id}</Id>
-          <Email>{post.email}</Email>
-          <Body>{post.body}</Body>
-        </Comment>
-      ))}
-    </Container>
+    <InfiniteScroll
+      dataLength={comments ? comments.length : 1}
+      next={getData}
+      loader={<></>}
+      hasMore={true}
+      scrollThreshold="1000px"
+    >
+      <Container>
+        <GlobalStyle />
+        {comments.map((post, index) => (
+          <Comment key={index}>
+            <Id>
+              <span>Comment Id</span>
+              {post.id}
+            </Id>
+            <Email>
+              <span>Email</span> {post.email}
+            </Email>
+            <Body>
+              <span>Comment</span>
+              <p>{post.body}</p>
+            </Body>
+          </Comment>
+        ))}
+      </Container>
+    </InfiniteScroll>
   );
 }
 
-export default App;
+export default CommentPosts;
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color:yellow;
-    font-family: "Helvetica", "Arial", sans-serif;
-    line-height: 1.5;
+    font-size:18px;
+    font-family: SFProDisplay;
   }
 `;
 const Comment = styled.div`
-  border: 1px solid black;
+  width: 500px;
+  height: 193px;
+  background: #f8f9fa;
+  border: 0.5px solid #ced4da;
+  box-sizing: border-box;
+  border-radius: 20px;
   margin: 30px;
 `;
 const Id = styled.div`
-  border: 1px solid red;
+  margin-top: 20px;
+  span {
+    font-weight: 900;
+    margin: 0 12px;
+  }
 `;
 const Email = styled.div`
-  border: 1px solid blue;
+  margin-top: 12px;
+  span {
+    font-weight: 900;
+    margin: 0 12px;
+  }
 `;
 const Body = styled.div`
-  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  margin-top: 12px;
+  span {
+    font-weight: 900;
+    margin: 0 12px;
+  }
+  p {
+    margin: 0 12px;
+  }
 `;
